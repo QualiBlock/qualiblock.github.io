4da87765078bbfd156279a840e5823bbb90a3e94
Evekiio <Founder@QualiBlock.tech>
2023-09-17 11:37:38 -0400

    fix style build and references

---
+++ public/_nuxt/custom.ae597846.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_plugin-vue_export-helper.c27b6911.js";import{c as s,M as o,F as t,o as n}from"./entry.a3a103fc.js";const a={};function c(e,l){return n(),s(t,null,[o(e.$slots,"header"),o(e.$slots,"main"),o(e.$slots,"footer")],64)}const _=r(a,[["render",c]]);export{_ as default};
